8fedc10bda5088c56b7f9e1a5dd9e681b3d5120c
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2025-08-25 09:46:05 +0700

    Update modul
    Update modul

--- routes/modul.js
+++ routes/modul.js
@@ -5,18 +5,43 @@ const router = express.Router();
 const pdfList = [
   {
     id: 1,
-    title: "Economic Report 2024",
-    pdfPath: "/pdf/laporan1.pdf"
+    title: "Modul 1. Misi Ibu Cerdas Di Era Digital.pdf",
+    pdfPath: "/pdf/Modul 1. Misi Ibu Cerdas Di Era Digital.pdf"
   },
   {
     id: 2,
-    title: "Stock Market Overview Q2",
-    pdfPath: "/pdf/laporan2.pdf"
+    title: "Modul 2. Mengenal Literasi Keuangan Keluarga.pdf",
+    pdfPath: "/pdf/Modul 2. Mengenal Literasi Keuangan Keluarga.pdf"
   },
   {
     id: 3,
-    title: "Macroeconomic Trends",
-    pdfPath: "/pdf/laporan3.pdf"
+    title: "Modul 3. Membedakan Kebutuhan atau Keinginan .pdf",
+    pdfPath: "/pdf/Modul 3. Membedakan Kebutuhan atau Keinginan.pdf"
+  },
+  {
+    id: 4,
+    title: "Modul 4, Menyusun Anggaran dan Mencatat Keuangan.pdf",
+    pdfPath: "/pdf/Modul 4, Menyusun Anggaran dan Mencatat Keuangan.pdf"
+  },
+  {
+    id: 5,
+    title: "Modul 5. Menabung dan Berinvestasi.pdf",
+    pdfPath: "/pdf/Modul 5. Menabung dan Berinvestasi.pdf"
+  },
+  {
+    id: 6,
+    title: "Modul 6. Deteksi dini dan komunikasi keluarga (Teknik pendekatan suportif dan cara menghindari konflik saat menghadapi isu ini).pdf",
+    pdfPath: "/pdf/Modul 6. Deteksi dini dan komunikasi keluarga (Teknik pendekatan suportif dan cara menghindari konflik saat menghadapi isu ini).pdf"
+  },
+  {
+    id: 9,
+    title: "Modul 9. Ibu sebagai agen keuangan positif dalam keluarga (Membangun peran edukatif dan cerita sukses ibu yang mengubah pola kauangan).pdf",
+    pdfPath: "/pdf/Modul 9. Ibu sebagai agen keuangan positif dalam keluarga (Membangun peran edukatif dan cerita sukses ibu yang mengubah pola kauangan).pdf"
+  },
+  {
+    id: 10,
+    title: "Modul 10. Refleksi &  Rencana Aksi Keluarga Cerdas Finansial.pdf",
+    pdfPath: "/pdf/Modul 10. Refleksi &  Rencana Aksi Keluarga Cerdas Finansial.pdf"
   }
 ];
 
